c64bacebde219edf63fdee1a8acc9140815dd86a
NoahCode <107689370+NoahCode1@users.noreply.github.com>
2022-10-05 18:19:19 -0300

    NoahCode-Linux-Setup

---
+++ NoahCode-Linux-Setup.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+sudo pacman -Syu
+
+sudo pacman -S vlc firefox wine lutris flatpak
+
+flatpak install flathub com.valvesoftware.Steam
+
+flatpak install flathub com.heroicgameslauncher.hgl
+
+flatpak install flathub com.obsproject.Studio
+
+flatpak install flathub md.obsidian.Obsidian
+
+flatpak install flathub net.davidotek.pupgui2
